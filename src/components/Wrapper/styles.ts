56dca1c97f0347a66ab4868e071d2574ec6b2a0a
André carvalho <andre.vrlcarvalho@hotmail.com>
2023-08-25 10:35:24 -0300

    mutations e ajuste de tamanho do form

--- src/components/Wrapper/styles.ts
+++ src/components/Wrapper/styles.ts
@@ -2,7 +2,7 @@ import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
-    max-width: 80rem;
+    max-width: 110rem;
     margin: 8rem auto;
     background: ${theme.colors.white};
     padding: ${theme.spacings.xlarge};
---
+++ src/graphql/mutations/exercise-performance.ts
@@ -0,0 +1,79 @@
+import { gql } from 'graphql-request';
+import { GQL_FRAGMENT_EXERCISE_PERFORMANCE_ENTITY_RESPONSE } from '../fragments/exercise-performance';
+
+export const GQL_MUTATION_UPDATE_EXERCISE = gql`
+  ${GQL_FRAGMENT_EXERCISE_PERFORMANCE_ENTITY_RESPONSE}
+
+  mutation UPDATE_EXERCISE_PERFORMANCE(
+  $id: ID!,
+  $exercise: ID
+  $student: ID
+  $training: ID
+  $series: Int
+  $repetitionsExpected: Int
+  $repetitionsExecuted: Int
+  $loadOrTimeExpected: Int
+  $loadOrTimeExecuted: Int
+  $type: ENUM_EXERCISEPERFORMANCE_TYPE
+) {
+  updateExercisePerformance(
+    id: $id
+    data: {
+      series: $series
+      exercise: $exercise
+      student: $student
+      training: $training
+      repetitionsExpected: $repetitionsExpected
+      repetitionsExecuted: $repetitionsExecuted
+      loadOrTimeExpected: $loadOrTimeExpected
+      loadOrTimeExecuted: $loadOrTimeExecuted
+      type: $type
+    }
+  ) {
+    ...ExercisePerformanceEntityResponseFragment
+  }
+}
+
+`;
+
+export const GQL_MUTATION_CREATE_EXERCISE = gql`
+  ${GQL_FRAGMENT_EXERCISE_PERFORMANCE_ENTITY_RESPONSE}
+
+  mutation CREATE_EXERCISE_PERFORMANCE(
+  $exercise: ID
+  $student: ID
+  $training: ID
+  $series: Int
+  $repetitionsExpected: Int
+  $repetitionsExecuted: Int
+  $loadOrTimeExpected: Int
+  $loadOrTimeExecuted: Int
+  $type: ENUM_EXERCISEPERFORMANCE_TYPE
+) {
+  createExercisePerformance(
+    data: {
+      series: $series
+      exercise: $exercise
+      student: $student
+      training: $training
+      repetitionsExpected: $repetitionsExpected
+      repetitionsExecuted: $repetitionsExecuted
+      loadOrTimeExpected: $loadOrTimeExpected
+      loadOrTimeExecuted: $loadOrTimeExecuted
+      type: $type
+    }
+  ) {
+    ...ExercisePerformanceEntityResponseFragment
+  }
+}
+`;
+
+export const GQL_MUTATION_DELETE_EXERCISE_RESPONSE = gql`
+  ${GQL_FRAGMENT_EXERCISE_PERFORMANCE_ENTITY_RESPONSE}
+
+  mutation DELETE_EXERCISE_PERFORMANCE($id: ID!){
+    deleteExercisePerformance(id: $id){
+    ...ExerciseEntityResponseFragment
+  }
+}
+`;
